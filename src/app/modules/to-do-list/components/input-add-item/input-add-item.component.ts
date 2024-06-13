194fd38d84e9b82fd0acb675c6a16e69b2827aca
Fernando Sanches <Sanchesfernando.work@gmail.com>
2024-06-13 13:24:19 -0300

    atualizações

--- src/app/modules/to-do-list/components/input-add-item/input-add-item.component.ts
+++ src/app/modules/to-do-list/components/input-add-item/input-add-item.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+
+import { ChangeDetectorRef, Component, ElementRef, EventEmitter, inject, Input, input, Output, output, ViewChild, viewChild } from '@angular/core';
+import { IlistItems } from '../../interface/IListItems.interface';
+import { JsonPipe, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-input-add-item',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './input-add-item.component.html',
   styleUrl: './input-add-item.component.scss'
 })
 export class InputAddItemComponent {
+  #cdr = inject(ChangeDetectorRef);
 
+  @ViewChild("inputText")public inputText!: ElementRef;
+
+  @Input({ required: true }) public inputListItems: IlistItems[] = [];
+
+  @Output()public outputAddListItems = new EventEmitter<IlistItems>();
+  public focusAndAddItem(value:string){
+    if(value){
+      this.#cdr.detectChanges();
+      this.inputText.nativeElement.value = " ";
+
+      const currentDate = new Date();
+      const timestamp = currentDate.getTime();
+      const id = `ID ${timestamp}`
+
+      this.outputAddListItems.emit({
+        id,
+        checked: false,
+        value
+      })
+      return this.inputText.nativeElement.focus();
+
+    }
+  }
 }
--- src/app/modules/to-do-list/pages/list/list.component.ts
+++ src/app/modules/to-do-list/pages/list/list.component.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { InputAddItemComponent } from '../../components/input-add-item/input-add-item.component';
+import { IlistItems } from '../../interface/IListItems.interface';
 
 @Component({
   selector: 'app-list',
@@ -10,4 +11,16 @@ import { InputAddItemComponent } from '../../components/input-add-item/input-add
 })
 export class ListComponent {
   public addItem = signal(true);
+
+  #setListItems = signal<IlistItems[]>([this.#parserItems()]);
+  public getListItems = this.#setListItems.asReadonly();
+
+  #parserItems(){
+    return JSON.parse(localStorage.getItem('@my-list') || '[]')
+  }
+
+  public getInputAndAddItem(value: IlistItems){
+    localStorage.setItem('@my-list', JSON.stringify([...this.#setListItems(), value]));
+    return this.#setListItems.set(this.#parserItems())
+  }
 }
